7bbd8ba5beecb4b71ed173b810501e2443800a64
tvvizzle <jball070@fiu.edu>
2019-06-20 03:36:47 -0400

    Login actual logs in user
    needs testing better implementation will be coming soon

--- MDChem/Assets/mobile game menu kit/script/home scene/profiles/new_profile_window.cs
+++ MDChem/Assets/mobile game menu kit/script/home scene/profiles/new_profile_window.cs
@@ -42,7 +42,6 @@ public class new_profile_window : MonoBehaviour
 
         profile_slot = profile_target_slot;
 
-        //if (PlayerPrefs.GetString("ui") == null || PlayerPrefs.GetString("ui") == "")
         this.gameObject.SetActive(true);
 
 
@@ -79,11 +78,14 @@ public class new_profile_window : MonoBehaviour
 
         Debug.Log("uuid " + uuid);
         PlayerPrefs.SetString("ui", uuid);
+        Debug.Log("user has been logged in ended");
+
+        //set abunch of gui stuff
+        setGUI();
     }
 
     public Task CreateUserEmailAsync()
     {
-
         Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
         return auth.CreateUserWithEmailAndPasswordAsync(my_input.text, password.text)
             .ContinueWith(task =>
@@ -102,8 +104,8 @@ public class new_profile_window : MonoBehaviour
         Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
         return auth.SignInWithEmailAndPasswordAsync(my_input.text, password.text).ContinueWith(task =>
         {
-          //  StartCoroutine(authCallBack(task, "login"));
-           // Debug.Log("In Login");
+            //  StartCoroutine(authCallBack(task, "login"));
+            // Debug.Log("In Login");
             if (task.IsCompleted)
             {
                 var user = task.Result;
@@ -118,43 +120,49 @@ public class new_profile_window : MonoBehaviour
 
     public void Ok_button()
     {
-        Debug.Log("SIGNING UP USER");
-        // if (my_input.text != "")
-        // {
+
+        if (my_input.text != "")
+        {
+            Debug.Log("SIGNING UP USER");
             CreateAccount();
             Debug.Log("SIGNED UP USER");
-
-        //     int old_profile = my_game_master.current_profile_selected;
-        //     my_game_master.current_profile_selected = profile_slot;
-
-        //     if (my_manage_menu_uGUI.current_screen == my_manage_menu_uGUI.profile_screen)//update profile screen
-        //     {
-        //         my_profile_manager.Select_this_profile(old_profile);
-        //         my_profile_manager.Select_this_profile(profile_slot);
-        //     }
-
-        //     my_game_master.Gui_sfx(my_game_master.tap_sfx);
-        //     my_game_master.Create_new_profile(my_input.text);
-        //     my_input.text = "";
-        //     my_manage_menu_uGUI.Update_profile_name(true);
-        //     this.gameObject.SetActive(false);
-        //     my_profile_manager.Update_this_slot(my_game_master.current_profile_selected);
-        // }
-        // else
-        //     my_game_master.Gui_sfx(my_game_master.tap_error_sfx);
+            setGUI();
+        }
+        else
+            my_game_master.Gui_sfx(my_game_master.tap_error_sfx);
 
     }
 
     public void loginButton()
     {
-        Debug.Log("Attempting to login user");
-        LoginUser();
-        Debug.Log("USER LOGGED IN");
+        if (my_input.text != "")
+            LoginUser();
+        else
+            my_game_master.Gui_sfx(my_game_master.tap_error_sfx);
     }
 
     public void Cancel()
     {
         my_game_master.Gui_sfx(my_game_master.tap_sfx);
         this.gameObject.SetActive(false);
+    }
+
+    public void setGUI()
+    {
+        int old_profile = my_game_master.current_profile_selected;
+        my_game_master.current_profile_selected = profile_slot;
+
+        if (my_manage_menu_uGUI.current_screen == my_manage_menu_uGUI.profile_screen)//update profile screen
+        {
+            my_profile_manager.Select_this_profile(old_profile);
+            my_profile_manager.Select_this_profile(profile_slot);
+        }
+
+        my_game_master.Gui_sfx(my_game_master.tap_sfx);
+        my_game_master.Create_new_profile(my_input.text);
+        my_input.text = "";
+        my_manage_menu_uGUI.Update_profile_name(true);
+        this.gameObject.SetActive(false);
+        my_profile_manager.Update_this_slot(my_game_master.current_profile_selected);
     }
 }
